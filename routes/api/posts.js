d532540052b5f641548c9dbf13ff77ab9e99fc57
Joseph Mendoza <josephmendoza23@gmail.com>
2018-06-11 11:48:39 -0400

    implemented routes for finding all posts and finding a post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,6 +16,29 @@ const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "Posts route works" }));
 
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Public
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route    GET api/posts/:id
+// @desc     Get all posts
+// @access   Public
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
 // @route    POST api/posts
 // @desc     Create post
 // @access   Private
