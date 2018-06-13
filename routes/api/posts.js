7b7a780c0bb5c0d54a95026c194b48e7b50d368c
Joseph Mendoza <josephmendoza23@gmail.com>
2018-06-13 13:17:48 -0400

    created routes for unliking post, commenting posts, and deleting comments from posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -100,7 +100,7 @@ router.post(
     Profile.findOne({ user: req.user.id }).then(profile => {
       Post.findById(req.params.post_id).then(post => {
         if (
-          post.likes.filter(like => like.user.toString() === req.params.id)
+          post.likes.filter(like => like.user.toString() === req.user.id)
             .length > 0
         ) {
           return res
@@ -114,6 +114,101 @@ router.post(
         // Save
         post.save().then(post => res.json(post));
       });
+    });
+  }
+);
+
+// @route    POST api/posts/unlike/:post_id
+// @desc     Unlike a post by post id
+// @access   Private
+
+router.post(
+  "/unlike/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.post_id).then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length === 0
+        ) {
+          return res
+            .status(400)
+            .json({ notliked: "User has not liked this post" });
+        }
+
+        // Remove like
+        post.likes = post.likes.filter(
+          like => like.user.toString() !== req.user.id
+        );
+
+        // Save
+        post.save().then(post => res.json(post));
+      });
+    });
+  }
+);
+
+// @route    POST api/posts/comment/:post_id
+// @desc     Add a comment to a post
+// @access   Private
+
+router.post(
+  "/comment/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.post_id)
+        .then(post => {
+          const newComment = {
+            user: req.user.id,
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar
+          };
+
+          // Add to comments array
+          post.comments.unshift(newComment);
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err =>
+          res.status(404).status.json({ postnotfound: "Post not found" })
+        );
+    });
+  }
+);
+
+// @route    Delete api/posts/comment/:post_id/:comment_id
+// @desc     Remove comment from post
+// @access   Private
+
+router.delete(
+  "/comment/:post_id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.post_id).then(post => {
+      // Check if comment exist
+      if (
+        post.comments.filter(c => c.id.toString() === req.params.comment_id)
+          .length === 0
+      ) {
+        return res
+          .status(404)
+          .json({ commentdoesnotexist: "Comment does not exist" });
+      }
+
+      // Delete comment
+      post.comments = post.comments.filter(c => c.id !== req.params.comment_id);
+      // Save
+      post.save().then(post => res.json(post));
     });
   }
 );
