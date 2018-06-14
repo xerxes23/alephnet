5bd45cd1e4a859c2bd1b478281d1ff00ca571823
Joseph Mendoza <josephmendoza23@gmail.com>
2018-06-14 19:07:29 -0400

    implemented react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+
+// Components
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
 import Landing from "./layout/Landing";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/layout/Footer.js
+++ client/src/layout/Footer.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-export default () => {
-  return (
-    <footer class="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} AlephNet
-    </footer>
-  );
-};
+export default () => (
+  <footer class="bg-dark text-white mt-5 p-4 text-center">
+    Copyright &copy; {new Date().getFullYear()} AlephNet
+  </footer>
+);
