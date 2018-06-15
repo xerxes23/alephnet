7544a16dfb69abef5118beaf7282456e5de9f62b
Joseph Mendoza <josephmendoza23@gmail.com>
2018-06-15 12:33:25 -0400

    enable frontend to make http call to register user

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Register extends Component {
   state = {
@@ -17,7 +18,11 @@ export default class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
+
+    axios
+      .post("/api/users/register", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   };
 
   onChange = e => {
