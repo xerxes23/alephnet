0e8c5a1b15c5cee30684fba552eda2f04b3bd485
xerxes23 <josephmendoza23@gmail.com>
2018-07-30 18:37:09 -0400

    Modified server file

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-// Serve static assets if in production
+// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", " build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
